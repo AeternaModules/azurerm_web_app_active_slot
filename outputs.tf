be81d9b83ec6190f3478fbf01a0f7302e24ccbb0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "web_app_active_slots" {
-  description = "All web_app_active_slot resources"
-  value       = azurerm_web_app_active_slot.web_app_active_slots
-}
 output "web_app_active_slots_last_successful_swap" {
-  description = "List of last_successful_swap values across all web_app_active_slots"
-  value       = [for k, v in azurerm_web_app_active_slot.web_app_active_slots : v.last_successful_swap]
+  description = "Map of last_successful_swap values across all web_app_active_slots, keyed the same as var.web_app_active_slots"
+  value       = { for k, v in azurerm_web_app_active_slot.web_app_active_slots : k => v.last_successful_swap }
 }
 output "web_app_active_slots_overwrite_network_config" {
-  description = "List of overwrite_network_config values across all web_app_active_slots"
-  value       = [for k, v in azurerm_web_app_active_slot.web_app_active_slots : v.overwrite_network_config]
+  description = "Map of overwrite_network_config values across all web_app_active_slots, keyed the same as var.web_app_active_slots"
+  value       = { for k, v in azurerm_web_app_active_slot.web_app_active_slots : k => v.overwrite_network_config }
 }
 output "web_app_active_slots_slot_id" {
-  description = "List of slot_id values across all web_app_active_slots"
-  value       = [for k, v in azurerm_web_app_active_slot.web_app_active_slots : v.slot_id]
+  description = "Map of slot_id values across all web_app_active_slots, keyed the same as var.web_app_active_slots"
+  value       = { for k, v in azurerm_web_app_active_slot.web_app_active_slots : k => v.slot_id }
 }
 
